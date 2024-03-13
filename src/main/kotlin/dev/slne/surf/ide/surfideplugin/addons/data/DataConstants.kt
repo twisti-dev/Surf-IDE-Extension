14c6e95b8af2be5211432ef66ff7ec47b6204bdd
twisti <twisti@slne.dev>
2024-03-13 08:07:07 +0100

    feat: Added data listener inspection

---
+++ src/main/kotlin/dev/slne/surf/ide/surfideplugin/addons/data/DataConstants.kt
@@ -0,0 +1,7 @@
+package dev.slne.surf.ide.surfideplugin.addons.data
+
+object DataConstants {
+    val DATA_LISTENER_ANNOTATION = "dev.slne.data.api.spring.redis.event.annotation.DataListener"
+    val DATA_LISTENERS_ANNOTATION = "dev.slne.data.api.spring.redis.event.annotation.DataListeners"
+    val DATA_REDIS_EVENT = "dev.slne.data.api.spring.redis.event.RedisEvent"
+}
